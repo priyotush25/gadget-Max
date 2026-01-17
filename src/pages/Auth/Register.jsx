a07d757c0807e7ad60e86318e938be64dbaf7d9f
Priyotush <priyotushpaul40@gmail.com>
2026-01-17 23:41:51 +0600

    register and photo upload

--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -3,11 +3,39 @@ import Container from "../../component/Container/Container";
 import Button from "../../component/Button/Button";
 import { Link } from "react-router";
 import { useForm } from "react-hook-form";
+import uploadImage from "../../utils/uploadImage";
+import useAuth from "../../hooks/useAuth";
+import { updateProfile } from "firebase/auth";
 
 const Register = () => {
   const { register, handleSubmit } = useForm();
+  const { handleRegister, GoogleSignIn } = useAuth();
 
-  const handleLogin = () => {};
+  // email register
+  const registerHandle = async (data) => {
+    try {
+      const imageURL = await uploadImage(data.image[0]);
+      const res = await handleRegister(data.email, data.password);
+
+      updateProfile(res.user, {
+        displayName: data.name,
+        photoURL: imageURL,
+      });
+
+      console.log("user register successfully");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // google register
+  const handleGoogleLogin = async () => {
+    try {
+      await GoogleSignIn();
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Container className="w-full flex items-center justify-center py-16">
@@ -16,7 +44,7 @@ const Register = () => {
           Create Account
         </h2>
 
-        <form className="space-y-4" onSubmit={handleSubmit(handleLogin)}>
+        <form className="space-y-4" onSubmit={handleSubmit(registerHandle)}>
           {/* Name */}
           <div>
             <label className="block mb-1 font-medium text-gray-700">Name</label>
@@ -24,6 +52,18 @@ const Register = () => {
               type="text"
               {...register("name", { required: true })}
               placeholder="Enter your full name"
+              className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-500"
+            />
+          </div>
+
+          {/* Image */}
+          <div>
+            <label className="block mb-1 font-medium text-gray-700">
+              Upload Photo
+            </label>
+            <input
+              type="file"
+              {...register("image", { required: true })}
               className="w-full px-4 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-green-500"
             />
           </div>
@@ -68,9 +108,9 @@ const Register = () => {
           </div>
 
           {/* Register Button */}
-          <Button className="w-full text-center bg-green-600 hover:bg-green-700 text-white py-2 rounded mt-2">
+          <button className="w-full text-center cursor-pointer bg-green-600 hover:bg-green-700 text-white py-2 rounded mt-2">
             Register
-          </Button>
+          </button>
         </form>
 
         {/* Divider */}
@@ -81,9 +121,9 @@ const Register = () => {
         </div>
 
         {/* Google Login */}
-        <Button
+        <button
           onClick={handleGoogleLogin}
-          className="w-full bg-gray-200 hover:bg-gray-300 text-gray-800 py-2 rounded flex items-center justify-center space-x-2"
+          className="w-full bg-gray-200 cursor-pointer hover:bg-gray-300 text-gray-800 py-2 rounded flex items-center justify-center space-x-2"
         >
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
@@ -91,7 +131,7 @@ const Register = () => {
             className="w-5 h-5"
           />
           <span>Register with Google</span>
-        </Button>
+        </button>
 
         {/* Extra */}
         <p className="text-sm text-center text-gray-500 mt-4">
