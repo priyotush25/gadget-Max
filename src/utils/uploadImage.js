c57cd1dfc4e323eece376f92980068f7981cfd68
Priyotush <priyotushpaul40@gmail.com>
2026-01-17 23:26:25 +0600

    update register

---
+++ src/utils/uploadImage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const uploadImage = async(imageFile) => {
+   const formData = new FormData();
+   formData.append("image", imageFile)
+
+
+   const url = `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMAGE_BB_KEY}`
+
+
+   const res = await fetch(url, {
+    method: "POST",
+    body: formData
+   })
+
+   const data = res.json();
+   return data.data.display_url;
+
+
+};
+
+export default uploadImage;
